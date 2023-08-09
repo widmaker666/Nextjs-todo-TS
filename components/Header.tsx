ddeb88099fe5ac855a763350dd68a1b9fb36f7ca
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-08-09 09:09:37 +0200

    send

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import {Flex, Heading, Text, Input, Button} from "@chakra-ui/react"
+
+const Header = () => {
+  return (
+    <>
+    
+    <Flex p="2rem" direction="column" alignItems="center">
+        <Heading as='h1' size="4x1" noOfLines={1} className="tasklist-title">
+            TaskList.io
+        </Heading>
+        <Text mt="1rem" className="tasklist-slogan">
+            TaskList is an utilities
+        </Text>
+    </Flex>
+
+    </>
+  )
+}
+
+export default Header
