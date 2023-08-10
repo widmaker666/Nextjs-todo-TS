f79668f1bb004dd55a6bcb50b9dc0f254051756f
widmaker666 <widmaker666@msn.com>
2023-08-10 14:24:26 +0200

    maj css

--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -6,21 +6,31 @@ import { AddTaskPropos } from "@/types";
 const AddTask = ({ task, setTask, handleCreateTask }: AddTaskPropos) => {
   return (
     <>
-    <div className="input-container">
-      label
-    </div>
-      <Flex pt="2rem" pl="2rem" pr="2rem" pb="1rem">
+      <Flex
+        pt="2rem"
+        pl="2rem"
+        pr="2rem"
+        pb="1rem"
+        margin="10px auto"
+        width="500px"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Input
           placeholder="Add a task"
           size="lg"
+          mr="1rem"
+          background="white"
           value={task}
           onChange={(e) => setTask(e.target.value)}
-          style={{'background': "white"}}
         />
-        <Button colorScheme="twitter"
-        size="lg"
-        onClick={()=> handleCreateTask()}>
-            <SmallAddIcon />
+        <Button
+          colorScheme="twitter"
+          size="lg"
+          onClick={() => handleCreateTask()}
+        >
+          <SmallAddIcon />
         </Button>
       </Flex>
     </>
